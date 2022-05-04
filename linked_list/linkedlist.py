984a72f1076f68c00fe98f2e4f22e29ec141b498
Darya Matveeva <darya.d.chernikova@gmail.com>
2022-05-04 09:29:09 +0300

    linked list added

---
+++ linked_list/linkedlist.py
@@ -0,0 +1,109 @@
+class Node:
+
+    def __init__(self, v):
+        self.value = v
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_in_tail(self, item):
+        if self.head is None:
+            self.head = item
+        else:
+            self.tail.next = item
+        self.tail = item
+
+    def print_all_nodes(self):
+        node = self.head
+        while node != None:
+            print(node.value)
+            node = node.next
+
+    def find(self, val):
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                return node
+            node = node.next
+        return None
+
+    def find_all(self, val):
+        found_elements = list()
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                found_elements.append(node)
+            node = node.next
+        return found_elements
+
+    def delete(self, val, all=False):
+        # empty ll
+        if self.head is None:
+            return
+
+        if self.head.next is None:
+            if self.head.value == val:
+                self.head = None
+                return
+
+        if self.head.value == val:
+            self.head = self.head.next
+            if not all:
+                return
+
+        node = self.head
+        while node.next is not None:
+            if node.next.value == val:
+                node.next = node.next.next
+                if not all:
+                    return
+            node = node.next
+
+    def clean(self):
+        self.head = None
+        self.tail = None
+
+    def len(self):
+        length = 0
+        node = self.head
+        while node is not None:
+            length += 1
+            node = node.next
+        return length
+
+    def insert(self, afterNode, newNode):
+        if afterNode is None:
+            self.head = newNode
+        node = self.head
+        while node is not None:
+            if node is afterNode:
+                newNode.next = node.next
+                node.next = newNode
+                return
+            node = node.next
+
+    def __eq__(self, other):
+        if self.head is None and other.head is None:
+            return True
+        if self.len() != other.len():
+            return False
+        else:
+            node_from_self = self.head
+            node_from_other = other.head
+            while node_from_other is not None:
+                if node_from_self.value != node_from_other.value:
+                    return False
+                node_from_other = node_from_other.next
+                node_from_self = node_from_self.next
+        return True
+
+    def fill_from_array(self, values):
+        for value in values:
+            node = Node(value)
+            self.add_in_tail(node)
+        return self
---
+++ linked_list/linkedlist_tests.py
@@ -0,0 +1,117 @@
+import unittest
+from linkedlist import LinkedList
+from linkedlist import Node
+
+class MyTestCase(unittest.TestCase):
+
+    def test_equals_empty_ll(self):
+        empty_ll_1 = LinkedList()
+        empty_ll_2 = LinkedList()
+        self.assertEqual(empty_ll_1, empty_ll_2)
+
+    def test_equals_same_ll(self):
+        ll_1 = LinkedList().fill_from_array([1,1,1,4,0])
+        ll_2 = LinkedList().fill_from_array([1,1,1,4,0])
+        self.assertEqual(ll_1, ll_2)
+
+    def test_equals_not_same(self):
+        ll_1 = LinkedList().fill_from_array([1, 1])
+        ll_2 = LinkedList().fill_from_array([1, 1, 1, 4, 0])
+        self.assertNotEqual(ll_1, ll_2)
+
+
+    def test_delete_from_empty_ll(self):
+        ll = LinkedList()
+        ll.delete(1)
+        self.assertEqual(ll, LinkedList())
+
+    def test_delete_none_from_empty_ll(self):
+        ll = LinkedList()
+        ll.delete(None)
+        self.assertEqual(ll, LinkedList())
+
+    def test_delete_present_element_from_single_element_ll(self):
+        ll = LinkedList().fill_from_array([1])
+        ll.delete(1)
+        self.assertEqual(ll, LinkedList())
+
+    def test_delete_not_present_element_from_single_element_ll(self):
+        ll = LinkedList().fill_from_array([1])
+        ll.delete(2)
+        self.assertEqual(ll, LinkedList().fill_from_array([1]))
+
+    def test_delete_present_element_from_big_ll(self):
+        ll = LinkedList().fill_from_array([k for k in range(1,10000)])
+        ll.delete(20)
+
+        array_2 = []
+        for k in range(1, 10000):
+            if (k != 20):
+                array_2 += [k]
+        ll_2 = LinkedList().fill_from_array(array_2)
+        self.assertEqual(ll, ll_2)
+
+    def test_find_all_empty_ll(self):
+        ll = LinkedList()
+        result = ll.find_all(0)
+        self.assertEqual(result, [])
+
+    def test_find_all_none_in_empty_ll(self):
+        ll = LinkedList().fill_from_array([None])
+        result = ll.find_all(None)
+        self.assertEqual(result, [ll.head])
+
+    def test_find_all(self):
+        ll = LinkedList().fill_from_array([1,1,2,3,4,1])
+        result = ll.find_all(1)
+        ll.delete(2)
+        ll.delete(3)
+        ll.delete(4)
+        list_test = list()
+        node = ll.head
+        while node is not None:
+            list_test.append(node)
+            node = node.next
+        self.assertEqual(result, list_test)
+
+    def test_find_all_in_big_ll(self):
+        array = [1] * 1000
+        ll = LinkedList().fill_from_array(array)
+        result = ll.find_all(1)
+        list_test = list()
+        node = ll.head
+        while node is not None:
+            list_test.append(node)
+            node = node.next
+        self.assertEqual(result, list_test)
+
+    def test_len(self):
+        array = [1] * 1000
+        ll = LinkedList().fill_from_array(array)
+        self.assertEqual(ll.len(), 1000)
+
+    def test_insert_1(self):
+        node1 = Node(1)
+        node2 = Node(2)
+        node3 = Node(3)
+        ll = LinkedList()
+        ll.add_in_tail(node1)
+        ll.add_in_tail(node2)
+        ll.add_in_tail(node3)
+        ll.insert(node3, Node(4))
+        self.assertEqual(ll, LinkedList().fill_from_array([1, 2, 3, 4]))
+
+    def test_insert_2(self):
+        node1 = Node(1)
+        node2 = Node(2)
+        node3 = Node(3)
+        ll = LinkedList()
+        ll.add_in_tail(node1)
+        ll.add_in_tail(node2)
+        ll.add_in_tail(node3)
+        ll.insert(node2, Node(4))
+        self.assertEqual(ll, LinkedList().fill_from_array([1, 2, 4, 3]))
+
+
+if __name__ == '__main__':
+    unittest.main()
