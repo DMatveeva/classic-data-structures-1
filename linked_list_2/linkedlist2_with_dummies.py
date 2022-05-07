24c54ef83997506c0d7377a712934617dfadb102
Darya Matveeva <darya.d.chernikova@gmail.com>
2022-05-07 10:23:41 +0300

    linkedlist2 with dummies updated

--- linked_list_2/linkedlist2_with_dummies.py
+++ linked_list_2/linkedlist2_with_dummies.py
@@ -9,14 +9,17 @@ class Node:
 
 
 class DummyNode(Node):
+    def __init__(self):
+        super().__init__("This is error. You shouldn't see this, it's a dummy node.")
+
     def is_real(self):
         return False
 
 
 class LinkedList2:
     def __init__(self):
-        dummy_head = DummyNode(None)
-        dummy_tail = DummyNode(None)
+        dummy_head = DummyNode()
+        dummy_tail = DummyNode()
         dummy_head.prev = None
         dummy_head.next = dummy_tail
         dummy_tail.prev = dummy_head
@@ -25,29 +28,32 @@ class LinkedList2:
         self.tail = dummy_tail
 
     def add_in_tail(self, item):
-        self.insert(self.tail.prev, item)
+        self.tail.prev.next = item
+        item.prev = self.tail.prev
+        item.next = self.tail
+        self.tail.prev = item
 
     def find(self, val):
-        node = self.head.next
-        while node is not self.tail:
-            if node.value == val:
+        node = self.head
+        while node is not None:
+            if node.value == val and node.is_real():
                 return node
             node = node.next
         return None
 
     def find_all(self, val):
         result = []
-        node = self.head.next
-        while node is not self.tail:
-            if node.value == val:
+        node = self.head
+        while node is not None:
+            if node.value == val and node.is_real():
                 result.append(node)
             node = node.next
         return result
 
     def delete(self, val, all=False):
-        node = self.head.next
-        while node is not self.tail:
-            if node.value == val:
+        node = self.head
+        while node is not None:
+            if node.value == val and node.is_real():
                 node.prev.next = node.next
                 node.next.prev = node.prev
                 if not all:
@@ -67,11 +73,13 @@ class LinkedList2:
         return length
 
     def insert(self, afterNode, newNode):
-        if afterNode is None:
-            afterNode = self.head
+        if afterNode is None and self.head is None:
+            self.add_in_head(newNode)
+        elif afterNode is None and self.head is not None:
+            self.add_in_tail(newNode)
         node = self.head
         while node is not self.tail:
-            if node == afterNode:
+            if node == afterNode and node.is_real():
                 afterNode.next.prev = newNode
                 newNode.next = afterNode.next
                 newNode.prev = afterNode
@@ -79,6 +87,36 @@ class LinkedList2:
             node = node.next
 
     def add_in_head(self, newNode):
-        self.insert(None, newNode)
+        self.head.next.prev = newNode
+        newNode.next = self.head.next
+        newNode.prev = self.head
+        self.head.next = newNode
 
+    def fill(self, *values):
+        for value in values:
+            node = Node(value)
+            self.add_in_tail(node)
+        return self
 
+    def __eq__(self, other):
+        if self.head is None and other.head is None:
+            return True
+        if self.len() != other.len():
+            return False
+        else:
+            node_from_self = self.head
+            node_from_other = other.head
+            while node_from_other is not None:
+                if node_from_self.value != node_from_other.value:
+                    return False
+                node_from_other = node_from_other.next
+                node_from_self = node_from_self.next
+        return True
+
+    def __str__(self):
+        node = self.head
+        string = ''
+        while node is not None:
+            string += f'{node.value}, '
+            node = node.next
+        return string
