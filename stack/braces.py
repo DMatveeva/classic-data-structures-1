dc8479ba8213147c99025a3909402fdd4d19739c
Darya Matveeva <darya.d.chernikova@gmail.com>
2022-05-09 22:52:15 +0300

    braces updated

--- stack/braces.py
+++ stack/braces.py
@@ -5,14 +5,9 @@ def are_braces_balanced(string):
     stack = Stack()
 
     for brace in string:
-        if brace not in ('(', ')'):
-            raise Exception('String should consists of braces only.')
         if brace == '(':
             stack.push(0)
         elif brace == ')' and stack.pop() is None:
             return False
 
-    if stack.size() > 0:
-        return False
-    else:
-        return True
+    return stack.size() == 0
