68393761e0b2778290c94358fb820ae51666ecc9
Darya Matveeva <darya.d.chernikova@gmail.com>
2022-05-11 22:08:13 +0300

    refactor rotate

--- queue/rotate_queue.py
+++ queue/rotate_queue.py
@@ -1,5 +1,5 @@
 def rotate_queue(queue, n):
-    i = n % queue.size() - queue.size()
+    i = 0 if n % queue.size() == 0 else n % queue.size() - queue.size()
     while i < 0:
         queue.enqueue(queue.dequeue())
         i += 1
